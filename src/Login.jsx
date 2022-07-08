a4ad0e50f46e5654fa90de72d19cc91647c05d57
Steve LeValley <slevalley@gmail.com>
2022-07-08 08:03:35 -0700

    lint cleanup

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,53 +1,53 @@
-import React, { useState } from "react";
-import "./Login.css";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import './Login.css'
+import { Link } from 'react-router-dom'
 // import { auth } from "./firebase";
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function Login () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const signIn = (e) => {
-    e.preventDefault();
+  const signIn = e => {
+    e.preventDefault()
     // firebase login functionality
-  };
+  }
 
-  const register = (e) => {
-    e.preventDefault();
+  const register = e => {
+    e.preventDefault()
     // firebase register account functionality
-  };
+  }
 
   return (
-    <div className="login">
-      <Link to="/">
+    <div className='login'>
+      <Link to='/'>
         <img
-          className="login__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-          alt=""
+          className='login__logo'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
+          alt=''
         />
       </Link>
 
-      <div className="login__container">
+      <div className='login__container'>
         <h1>Sign-in</h1>
 
         <form>
           <h5>Email</h5>
           <input
-            type="text"
+            type='text'
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value)}
           />
 
           <h5>Password</h5>
           <input
-            type="password"
+            type='password'
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={e => setPassword(e.target.value)}
           />
 
           <button
-            type="submit"
-            className="login__signInButton"
+            type='submit'
+            className='login__signInButton'
             onClick={signIn}
           >
             Sign In
@@ -60,12 +60,12 @@ function Login() {
           Interest-Based Ads Notice.
         </p>
 
-        <button className="login__registerButton" onClick={register}>
+        <button className='login__registerButton' onClick={register}>
           Create your Amazon Account
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
