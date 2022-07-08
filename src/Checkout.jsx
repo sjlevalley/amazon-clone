369d7601102148488b1ee061ca08cb543e22af4e
Steve LeValley <slevalley@gmail.com>
2022-07-07 20:59:45 -0700

    Updates

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import Subtotal from './Subtotal'
-import './Checkout.css'
+import React from "react";
+import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
+import "./Checkout.css";
 
-function Checkout () {
+function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
-    <div className='checkout'>
-      <div className='checkout__left'>
+    <div className="checkout">
+      <div className="checkout__left">
         <img
-          className='checkout__ad'
-          src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
-          alt=''
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
         />
-        <h2 className='checkout__title'>Your shopping Basket</h2>
+        <h2 className="checkout__title">Your shopping Basket</h2>
+        {basket.map((item) => (
+          <CheckoutProduct
+            id={item.id}
+            image={item.image}
+            title={item.title}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
       </div>
-      <div className='checkout__right'>
+      <div className="checkout__right">
         <Subtotal />
       </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
